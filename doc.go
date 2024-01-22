11482871f01fae6640972d7997d93479fbce01f4
Joey Tatú <joeyctatu@gmail.com>
2024-01-22 00:23:14 +0000

    Updated doc with gofmt

--- doc.go
+++ doc.go
@@ -1,37 +1,37 @@
-// Package mypackage provides a utility for retrieving weather warnings from a specified URL.
+package weatherwarnings
+
+// Package weatherwarnings provides a utility for retrieving weather warnings from a specified URL.
 // It includes a function GetWarnings that returns a slice of Warning structs.
 //
 // Example usage:
 // package main
-
-// 		import (
-// 			"bufio"
-// 			"fmt"
-// 			"log"
 //
-// 			"github.com/JoeyTatu/weatherwarnings"
-// 		)
+// import (
+//     "bufio"
+//     "fmt"
+//     "log"
 //
-// 		func main() {
+//     "github.com/JoeyTatu/weatherwarnings"
+// )
 //
-// 		// Valid requests:
-// 		// weatherwarnings.GetWarnings("today")
-// 		// weatherwarnings.GetWarnings("tomorrow")
-// 		// weatherwarnings.GetWarnings("dayAfterTomorrow")
-// 		// weatherwarnings.GetWarnings() // defaults to "today"
-
-// 		weatherWarnings, err := weatherwarnings.GetWarnings("today")
-// 		if err != nil {
-// 			log.Fatal(err)
-// 		}
-
-// 		// Print or use the warnings as needed
-// 		for _, w := range weatherWarnings {
-// 			fmt.Printf("Title: %s\n", w.Title)
-// 			fmt.Printf("Description: %s\n", w.Description)
-// 			fmt.Printf("Valid: %s\n", w.Valid)
-// 			fmt.Printf("Issued: %s\n", w.Issued)
-// 			fmt.Println("-----------")
-// 		}
+// func main() {
+//     // Valid requests:
+//     // weatherwarnings.GetWarnings("today")
+//     // weatherwarnings.GetWarnings("tomorrow")
+//     // weatherwarnings.GetWarnings("dayAfterTomorrow")
+//     // weatherwarnings.GetWarnings() // defaults to "today"
+//
+//     weatherWarnings, err := weatherwarnings.GetWarnings("today")
+//     if err != nil {
+//         log.Fatal(err)
+//     }
+//
+//     // Print or use the warnings as needed
+//     for _, w := range weatherWarnings {
+//         fmt.Printf("Title: %s\n", w.Title)
+//         fmt.Printf("Description: %s\n", w.Description)
+//         fmt.Printf("Valid: %s\n", w.Valid)
+//         fmt.Printf("Issued: %s\n", w.Issued)
+//         fmt.Println("-----------")
+//     }
 // }
-package weatherwarnings

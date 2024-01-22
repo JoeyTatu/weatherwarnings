964ebf4ec0e0e870e8caba4d9de2f358c5128727
Joey Tatú <joeyctatu@gmail.com>
2024-01-22 00:14:40 +0000

    Updated code for A rating in Go Report Card

--- retrive_weather.go
+++ retrive_weather.go
@@ -1,7 +1,6 @@
 package weatherwarnings
 
 import (
-	"fmt"
 	"net/http"
 	"strings"
 	"time"
@@ -18,34 +17,36 @@ type Warning struct {
 
 func getDayAfterTomorrow() (string, error) {
 	currentTime := time.Now()
-
 	dayAfterTomorrow := currentTime.Add(48 * time.Hour)
 	nameOfDay := dayAfterTomorrow.Weekday().String()
 	nameOfDay = strings.ToLower(nameOfDay)
-
 	return nameOfDay, nil
 }
 
-func GetWarnings(day ...string) ([]Warning, error) {
-	var nameOfDay string
-
-	if len(day) == 0 {
-		nameOfDay = "today"
-	} else if day[0] == "tomorrow" {
-		nameOfDay = "tomorrow"
-	} else if day[0] == "dayAfterTomorrow" {
+func getURL(day string) (string, error) {
+	switch day {
+	case "today":
+		return "https://www.met.ie/warnings-today.html", nil
+	case "tomorrow":
+		return "https://www.met.ie/warnings-tomorrow.html", nil
+	case "dayAfterTomorrow":
 		result, err := getDayAfterTomorrow()
 		if err != nil {
-			return nil, err
+			return "", err
 		}
-		nameOfDay = result
-	} else {
-		return nil, fmt.Errorf("invalid day name")
+		return "https://www.met.ie/warnings-" + result + ".html", nil
+	default:
+		return "https://www.met.ie/warnings-today.html", nil
+	}
+}
+
+func GetWarnings(day ...string) ([]Warning, error) {
+	nameOfDay, err := getURL(day[0])
+	if err != nil {
+		return nil, err
 	}
 
-	url := "https://www.met.ie/warnings-" + nameOfDay + ".html"
-
-	response, err := http.Get(url)
+	response, err := http.Get(nameOfDay)
 	if err != nil {
 		return nil, err
 	}
@@ -71,18 +72,15 @@ func GetWarnings(day ...string) ([]Warning, error) {
 
 		statusSpan := s.Next().Find("span.sr-only")
 		if statusSpan != nil {
-
 			warning.Description = strings.TrimSpace(strings.ReplaceAll(statusSpan.Text(), "<br />", "\n"))
 		}
 
 		strongElement := s.Next().Find("strong.sr-only")
 		if strongElement != nil {
-
 			warning.Description = strings.TrimSpace(strings.ReplaceAll(strongElement.Text(), "<br />", "\n"))
 		}
 
 		s.Next().Find("p").Each(func(j int, p *goquery.Selection) {
-
 			text := strings.TrimSpace(strings.ReplaceAll(p.Text(), "<br />", "\n"))
 			switch {
 			case strings.Contains(text, "Valid"):
